cf5d8064dacc06ddf3883a931c37d6cceaa067c1
Nazar2602n <104212004+Nazar216@users.noreply.github.com>
2022-10-12 18:12:25 +0300

    2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,22 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const element = document.querySelector('ul')
+console.log(element)
+
+const createList = document.createElement('li')
+// console.log(createList)
+
+createList.classList.add('item')
+
+element.append(createList)
+
+for (let i = 0; i < ingredients.length; i++) {
+  createList.textContent = ingredients[0]
+  
+}
+
+
+
+
